a04a71497a83462ad58b236a387e35b6defd1606
KER AND HAN <shalcozbi@gmail.com>
2025-05-27 23:23:36 +0800

    quiz_creator.py pseudocode

---
+++ quiz_creator_oop/quiz_creator.py
@@ -0,0 +1,12 @@
+#Import the Question class to create quiz questions
+#Import the QuizFileWriter class to handle writing questions to a file
+# Create a QuizFileWriter class that will save questions to the given file
+#Run the interactive quiz tool
+#Greet user
+#Start a loop to collect questioms
+#Ask user to input question
+#Check if the entered correct answer is valid
+#Save the answer if valid
+#otherwise show an error message
+#ask user to if they want to input another question
+#if not "yes", end the loop

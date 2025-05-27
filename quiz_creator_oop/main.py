9806c732ac0058116b8827f4efe9774ed6a4e8b0
KER AND HAN <shalcozbi@gmail.com>
2025-05-27 23:40:53 +0800

    main.py of quiz_creator pseudocode

---
+++ quiz_creator_oop/main.py
@@ -0,0 +1,3 @@
+#Import the QuizCreator class so we can use it to build a quiz
+#This block only runs if this file is executed directly (not imported as a module)
+#Start the interactive quiz creation process

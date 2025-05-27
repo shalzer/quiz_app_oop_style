a0e52590d99420d12cb25966a72d55bbe2d5066d
KER AND HAN <shalcozbi@gmail.com>
2025-05-28 00:53:17 +0800

    main.py in quiz_app pseudocode

---
+++ quiz_app_oop/main.py
@@ -0,0 +1,9 @@
+# Import tkinter for GUI
+# Import DataLoader to load questions from file
+# Import the main QuizApp GUI class
+# Load quiz questions from a file into a list of dictionaries
+# If no questions were loaded, print a message and do not start the quiz
+# Create the main window for the quiz app
+# Pass the window and loaded questions to the QuizApp to start the GUI
+# Start the Tkinter event loop to keep the window running
+# Entry point: Run the main function when this script is executed

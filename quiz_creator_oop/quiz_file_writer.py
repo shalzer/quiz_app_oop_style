5de491fa100a064b3c54c082a08627b3e8f687e6
KER AND HAN <shalcozbi@gmail.com>
2025-05-27 23:10:59 +0800

    quiz_file_write.py pseudocode

---
+++ quiz_creator_oop/quiz_file_writer.py
@@ -0,0 +1,7 @@
+#Import the Question class so we can accept Question objects as input
+#Store filename
+#Open the file in append mode ("a") so we add to the end of the file
+#Write the question text to the file
+#Write each of the multiple choice options (A–D)
+#Write the correct answer
+#Add separator

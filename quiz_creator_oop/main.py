2ac8cf96e51a8bfc789e2f4d51c26d531be8c082
KER AND HAN <shalcozbi@gmail.com>
2025-05-27 23:41:33 +0800

    import QuizCreator class

--- quiz_creator_oop/main.py
+++ quiz_creator_oop/main.py
@@ -1,3 +1,5 @@
 #Import the QuizCreator class so we can use it to build a quiz
 #This block only runs if this file is executed directly (not imported as a module)
 #Start the interactive quiz creation process
+
+from quiz_creator import QuizCreator

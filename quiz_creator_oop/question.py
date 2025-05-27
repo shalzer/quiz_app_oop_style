0d4db41779d5980b5c286544201ff6e6404b092e
KER AND HAN <shalcozbi@gmail.com>
2025-05-27 22:58:05 +0800

    question.py pseudocode

---
+++ quiz_creator_oop/question.py
@@ -0,0 +1,6 @@
+# Initialize a question object with the question text, four choices, and the correct answer
+#Store option A, B, C, and D
+#Convert the correct answer into upper case and store it
+#Check if the correct answer is a valid choice
+#Return if true
+#Otherwise, return False

601624d527495f480c3dcc7dccdf8116ebdd8363
KER AND HAN <shalcozbi@gmail.com>
2025-05-28 00:12:16 +0800

    initialize question text, choices, and answer key

--- quiz_app/question.py
+++ quiz_app/question.py
@@ -1,3 +1,9 @@
 #Initialize the question object with text, choices, and answer key
 #Check if the selected answer is correct
 # Return the correct answer key and its corresponding text
+
+class Question:
+    def __init__(self, question_text, choices_dict, answer_key):
+        self.question_text = question_text
+        self.choices_dict = choices_dict
+        self.answer_key = answer_key
